f0d3053ad3b307a5e8a4c7233d60dbafdb297601
LuisSoria31 <utm23090484@utma.edu.mx>
2026-07-24 18:05:06 -0600

    Fixed

--- dbcon.php
+++ dbcon.php
@@ -3,10 +3,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-$dbHost = $_ENV['DB_HOST'] ?? '127.0.0.1';
-$dbName = $_ENV['DB_NAME'] ?? 'commerce';
-$dbUser = $_ENV['DB_USER'] ?? 'root';
-$dbPass = $_ENV['DB_PASS'] ?? '';
+$dbHost = $_ENV['DB_HOST'] ?? 'datallizer.com';
+//$dbName = $_ENV['DB_NAME'] ?? 'commerce';//
+$dbName = $_ENV['DB_NAME'] ?? 'datallizer_ecommerce';
+$dbUser = $_ENV['DB_USER'] ?? 'datallizer_utmauser';
+$dbPass = $_ENV['DB_PASS'] ?? 'proyectoUtma2026';
 
 $dsn = "mysql:host=$dbHost;port=3306;dbname=$dbName;charset=utf8mb4";
 try {
